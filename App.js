5ed8eaa083c995b108cd1e6d67f68be72d3abb21
LeoSebastian23 <leosebastiang19@gmail.com>
2023-11-15 21:54:13 -0300

    Commit 001

--- App.js
+++ App.js
@@ -4,17 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Clase 2 - Proyecto con Expo - Developer LeoSebastian!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 1, 
+    backgroundColor: '#f0f0f0',
+    alignItems: 'center', 
     justifyContent: 'center',
-  },
+  }, 
 });
